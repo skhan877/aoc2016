1e91b4344cb7db0121c5724dd6209ebbe01ef3f0
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-20 22:54:07 +0000

    updates

--- day1.py
+++ day1.py
@@ -7,10 +7,21 @@ def parse_input(input):
     return arr
 
 def part_one(input):
+    n = len(input)
     x_moves = input[::2]
     y_moves = input[1::2]
     # return x_moves, y_moves
-    coords = (int(input[0][-1]), 0)
+    coords = [int(input[0][-1]), 0]
+
+    for i in range(1,4):
+        print(coords)
+        if input[i-1][0] == "L":
+            if input[i][0] == "L":
+                coords[1] -= int(input[i][1])
+            else:
+                coords[1] += int(input[i][1])
+
+
     return coords
 
 def part_two(input):
@@ -19,7 +30,7 @@ def part_two(input):
 def main():
     f = "inputs/day1.txt"
     data = parse_input(f)
-    # print(data)
+    print(data)
     print(part_one(data))
     print(part_two(data))
 
