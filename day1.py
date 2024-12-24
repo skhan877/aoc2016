b196351c975d00ec62b229d7f2ce914ba07c3d70
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-24 17:26:03 +0000

    updates

--- day1.py
+++ day1.py
@@ -8,44 +8,38 @@ def parse_input(input):
 
 def part_one(input):
     n = len(input)
-    x_moves = input[::2]
-    y_moves = input[1::2]
-    # return x_moves, y_moves
-
     if input[0][0] == "R": x = int(input[0][1])
     else: x = int(input[0][1]) * -1
     y = 0
-
-    # coords = [int(input[0][-1]), 0]
-
-    for i in range(1,10):
-        print(x, y)
+    for i in range(1, n):
+        # print(input[i], x, y)
         if i % 2 != 0: 
         # y axis
             if input[i-1][0] == "L":
                 if input[i][0] == "L": y -= int(input[i][1])
                 else: y += int(input[i][1])
             else:
-                if input[i][0] == "L": y -= int(input[i][1])
-                else: y += int(input[i][1])
+                if input[i][0] == "L": y += int(input[i][1])
+                else: y -= int(input[i][1])
         else:
         # x axis
             if input[i-1][0] == "L":
-                if input[i][0] == "L": x += int(input[i][1])
-                else: x -= int(input[i][1])
+                if input[i][0] == "L": x -= int(input[i][1])
+                else: x += int(input[i][1])
             else:
                 if input[i][0] == "L": y += int(input[i][1])
                 else: x -= int(input[i][1])
 
-    return ('final', x, y)
+    return (abs(x) + abs(y))
 
 def part_two(input):
     pass
 
 def main():
     f = "inputs/day1.txt"
-    data = parse_input(f)
-    print(data)
+    # data = parse_input(f)
+    # print(data)
+    data = ['R5', 'L5', 'R5', 'R3']
     print(part_one(data))
     print(part_two(data))
 
