c50aee3455241c3d2ae58fda3b32c0ddd898e04a
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-24 17:38:28 +0000

    updates

--- day1.py
+++ day1.py
@@ -7,39 +7,44 @@ def parse_input(input):
     return arr
 
 def part_one(input):
+    # print(input)
     n = len(input)
     if input[0][0] == "R": x = int(input[0][1])
     else: x = int(input[0][1]) * -1
     y = 0
     for i in range(1, n):
-        # print(input[i], x, y)
+        # print(input[i-1], x, y)
+        prev_dir = input[i-1][0]
+        cur_dir = input[i][0]
+        cur_move = input[i][1]
+
         if i % 2 != 0: 
         # y axis
-            if input[i-1][0] == "L":
-                if input[i][0] == "L": y -= int(input[i][1])
-                else: y += int(input[i][1])
+            if prev_dir == "L":
+                if cur_dir == "L": y -= int(cur_move)
+                else: y += int(cur_move)
             else:
-                if input[i][0] == "L": y += int(input[i][1])
-                else: y -= int(input[i][1])
+                if cur_dir == "L": y += int(cur_move)
+                else: y -= int(cur_move)
         else:
         # x axis
-            if input[i-1][0] == "L":
-                if input[i][0] == "L": x -= int(input[i][1])
-                else: x += int(input[i][1])
+            if prev_dir == "L":
+                if cur_dir == "L": x -= int(cur_move)
+                else: x += int(cur_move)
             else:
-                if input[i][0] == "L": y += int(input[i][1])
-                else: x -= int(input[i][1])
+                if cur_dir == "L": x += int(cur_move)
+                else: x -= int(cur_move)
 
-    return (abs(x) + abs(y))
+    return (x, y), abs(x) + abs(y)
 
 def part_two(input):
     pass
 
 def main():
     f = "inputs/day1.txt"
-    # data = parse_input(f)
+    data = parse_input(f)
     # print(data)
-    data = ['R5', 'L5', 'R5', 'R3']
+    # data = ['R5', 'L5', 'R5', 'R3']
     print(part_one(data))
     print(part_two(data))
 
