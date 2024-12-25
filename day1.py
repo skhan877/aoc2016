78d30458eeb7705b02230cb475674f5c258c5087
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-25 22:29:14 +0000

    updates

--- day1.py
+++ day1.py
@@ -6,37 +6,70 @@ def parse_input(input):
     arr = arr[0].split(", ")
     return arr
 
-def part_one(input):
-    # print(input)
-    n = len(input)
-    first_dir = input[0][0]
-    first_move = int(input[0][1:])
-    x = first_move if first_dir == "R" else first_move * -1
-    y = 0
-    for i in range(1, n):
-        print(input[i-1], (x, y), input[i])
-        prev_dir = input[i-1][0]
-        cur_dir = input[i][0]
-        cur_move = int(input[i][1:])
+# def part_one(input):
+#     # print(input)
+#     n = len(input)
+#     first_dir = input[0][0]
+#     first_move = int(input[0][1:])
+#     x = first_move if first_dir == "R" else first_move * -1
+#     y = 0
+#     for i in range(1, n):
+#         print(i, input[i-1], (x, y), input[i])
+#         prev_dir = input[i-1][0]
+#         cur_dir = input[i][0]
+#         cur_move = int(input[i][1:])
+#         if i % 2 != 0: 
+#         # y axis
+#             if prev_dir == "L":
+#                 if cur_dir == "L": 
+#                     y -= cur_move
+#                     choice = 1
+#                 else: 
+#                     y += cur_move
+#                     choice = 2
+#             else:
+#                 if cur_dir == "L": 
+#                     y += cur_move
+#                     choice = 3
+#                 else: 
+#                     y -= cur_move
+#                     choice = 4
+#         else:
+#         # x axis
+#             if prev_dir == "L":
+#                 if cur_dir == "L": 
+#                     x += cur_move
+#                     choice = 5
+#                 else: 
+#                     x -= cur_move
+#                     choice = 6
+#             else:
+#                 if cur_dir == "L": 
+#                     x -= cur_move
+#                     choice = 7
+#                 else: 
+#                     x += cur_move
+#                     choice = 8
+#         print(choice)
 
-        if i % 2 != 0: 
-        # y axis
-            if prev_dir == "L":
-                if cur_dir == "L": y -= cur_move
-                else: y += cur_move
-            else:
-                if cur_dir == "L": y += cur_move
-                else: y -= cur_move
+#     return (x, y), abs(x) + abs(y)
+
+def part_one(cmds):
+    dir = 0
+    pos, found = [0, 0, 0, 0], []
+    v = set()
+    for i in range(len(cmds)):
+        if cmds[i][0] == 'R':
+            dir = (dir + 1) % 4
         else:
-        # x axis
-            if prev_dir == "L":
-                if cur_dir == "L": x -= cur_move
-                else: x += cur_move
-            else:
-                if cur_dir == "L": x += cur_move
-                else: x -= cur_move
-
-    return (x, y), abs(x) + abs(y)
+            dir = (dir - 1) % 4
+        for j in range(int(cmds[i][1:])):
+            if (pos[0] - pos[2], pos[1] - pos[3]) not in v:
+                v.add((pos[0] - pos[2], pos[1] - pos[3]))
+            elif not found:
+                found = pos.copy()
+            pos[dir] += 1
+    return abs(pos[0] - pos[2]) + abs(pos[1] - pos[3])
 
 def part_two(input):
     pass
@@ -51,7 +84,7 @@ def main():
     # data = ['R5', 'L5', 'R5', 'R3']
     
     print(part_one(data))
-    print(part_two(data))
+    # print(part_two(data))
 
 if __name__ == "__main__":
     main() 
